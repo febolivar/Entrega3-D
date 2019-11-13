e860e0f1a14d01185397eb09c20897b3eefd5156
Fabio Bolívar <fe.bolivar@uniandes.edu.co>
2019-11-13 00:25:58 -0500

    comparacion

--- media/connect_mongo_db.py
+++ media/connect_mongo_db.py
@@ -81,10 +81,10 @@ def query_in_process():
         enterprises = db['users_enterprise']
 
         designs_to_process = list()
-        if design_id != 0:
+        if design_id != '0':
             print('a')
             print(design_id)
-        
+
         for item in designs.find({'id': int(design_id)}):
             # print('b')
             designs_to_process.append(
